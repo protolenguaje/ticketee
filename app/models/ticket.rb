df61dda05b5da2139123f2b446c00115b9d78495
Ivan <ivan.glez.aguilar@gmail.com>
2012-04-17 09:21:55 -0500

    Implemented creating tickets for a project

---
+++ app/models/ticket.rb
@@ -0,0 +1,6 @@
+class Ticket < ActiveRecord::Base
+  validates :title, :presence => true
+  validates :description, :presence => true,
+                          :length => { :minimum  => 10}
+  belongs_to :project
+end
