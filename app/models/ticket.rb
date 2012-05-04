6c35fa8fa9e1e4fc19c41d2a6422fe2f39b2f9ed
Ivan <ivan.glez.aguilar@gmail.com>
2012-05-04 10:28:41 -0500

    When creating tickets, attribute them to the creator.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,5 @@ class Ticket < ActiveRecord::Base
   validates :description, :presence => true,
                           :length => { :minimum  => 10}
   belongs_to :project
+  belongs_to :user
 end
